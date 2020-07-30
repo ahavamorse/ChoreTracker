d460491cedec323199068db8fb23f9065d1a365d
Ahava Morse <ahavamorse@icloud.com>
2020-07-30 12:38:07 -0700

    User struct

---
+++ ChoreTracker/ChoreTracker/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ChoreTracker
+//
+//  Created by Ahava Morse on 7/30/20.
+//  Copyright © 2020 Ahava Morse. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+}
